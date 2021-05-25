8372739e290f2db5ca969038e9293d4f4e6c3552
lvirgini <lvirgini@student.42.fr>
2021-05-25 10:20:39 +0200

    libft maths séparée de libft, norme v2

---
+++ includes/libft_math.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft_math.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvirgini <lvirgini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/17 00:09:18 by lvirgini          #+#    #+#             */
+/*   Updated: 2021/05/25 10:16:44 by lvirgini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_MATH_H
+# define LIBFT_MATH_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include "ft_maths.h"
+# include "ft_vector.h"
+
+#endif
